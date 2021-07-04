0ad80b0705b14ce3d25824110414c442e2b9abe1
Michael L Perry <michael@qedcode.com>
2021-07-03 21:08:44 -0500

    Compute the hash of a fact with an integer field

--- test/fact/knownHashSpec.ts
+++ test/fact/knownHashSpec.ts
@@ -10,7 +10,7 @@ describe("Known hash", () => {
         expect(hash).to.equal("uXcsBceLFAkZdRD71Ztvc+QwASayHA0Zg7wC2mc3zl28N1hKTbGBfBA2OnEHAWo+0yYVeUnABMn9MCRH8cRHWg==");
     });
 
-    it.only("Predecessor", () => {
+    it("Predecessor", () => {
         const hash = hashOf({
             type: "Skylane.Airline.Day",
             airline: {
@@ -20,7 +20,23 @@ describe("Known hash", () => {
             date: "2021-07-04T00:00:00.000Z"
         });
         expect(hash).to.equal("cQaErYsizavFrTIGjD1C0g3shMG/uq+hVUXzs/kCzcvev9gPrVDom3pbrszUsmeRelNv8bRdIvOb6AbaYrVC7w==");
-    })
+    });
+
+    it("Integer field", () => {
+        const hash = hashOf({
+            type: "Skylane.Flight",
+            airlineDay: {
+                type: "Skylane.Airline.Day",
+                airline: {
+                    type: "Skylane.Airline",
+                    identifier: "value"
+                },
+                date: "2021-07-04T00:00:00.000Z"
+            },
+            flightNumber: 4247
+        });
+        expect(hash).to.equal("PyXT7pCvBq7Vw63kEZGgbIVJxqA7jhoO+QbmeM3YC9laayG0gjln58khyOd4D/cmxXzocPaIuwXGWusVJxqEjQ==");
+    });
 });
 
 function hashOf(fact: HashMap) {
