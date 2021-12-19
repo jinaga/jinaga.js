a2998160724d1c4698162b2044f2a738f950ab10
Michael L Perry <michael@qedcode.com>
2021-12-18 18:56:15 -0600

    Better test output

--- integration-test/jinaga-test/jinaga.test.js
+++ integration-test/jinaga-test/jinaga.test.js
@@ -1,9 +1,11 @@
+const { expect } = require("chai");
 const { JinagaServer } = require("./jinaga");
 
 describe("Jinaga", () => {
     it("should save a fact", async () => {
         const { j } = JinagaServer.create({
-
+            pgKeystore: "postgresql://dev:devpw@localhost:5432/integrationtest",
+            pgStore:    "postgresql://dev:devpw@localhost:5432/integrationtest"
         });
 
         const root = await j.fact({
@@ -11,6 +13,14 @@ describe("Jinaga", () => {
             identifier: "test-root"
         });
 
-        expect(root.identifier).toBe("test-root");
+        expect(root.identifier).to.equal("test-root");
+    })
+
+    it("should pass", () => {
+        expect(1).to.equal(1)
+    })
+
+    it("should fail", () => {
+        expect(1).to.equal(2)
     })
 })
