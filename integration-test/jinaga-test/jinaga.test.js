5827bf05761183dc3643c6771013cb68734c1c69
Michael L Perry <michael@qedcode.com>
2021-12-18 20:19:59 -0600

    Use Docker Compose to run test and database in parallel

--- integration-test/jinaga-test/jinaga.test.js
+++ integration-test/jinaga-test/jinaga.test.js
@@ -4,8 +4,8 @@ const { JinagaServer } = require("./jinaga");
 describe("Jinaga", () => {
     it("should save a fact", async () => {
         const { j } = JinagaServer.create({
-            pgKeystore: "postgresql://dev:devpw@localhost:5432/integrationtest",
-            pgStore:    "postgresql://dev:devpw@localhost:5432/integrationtest"
+            pgKeystore: "postgresql://dev:devpw@db:5432/integrationtest",
+            pgStore:    "postgresql://dev:devpw@db:5432/integrationtest"
         });
 
         const root = await j.fact({
@@ -14,13 +14,5 @@ describe("Jinaga", () => {
         });
 
         expect(root.identifier).to.equal("test-root");
-    })
-
-    it("should pass", () => {
-        expect(1).to.equal(1)
-    })
-
-    it("should fail", () => {
-        expect(1).to.equal(2)
     })
 })
--- integration-test/run.sh
+++ integration-test/run.sh
@@ -5,4 +5,4 @@ cp ../dist/index.js ./jinaga-test/jinaga.js
 cp ../dist/index.js.map ./jinaga-test/jinaga.js.map
 docker build -t jinaga-test ./jinaga-test
 
-docker run -t jinaga-test
+docker compose up --exit-code-from jinaga-test
