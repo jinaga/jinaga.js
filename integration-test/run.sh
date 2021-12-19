dc0f708e0aa788e6cbbe40e577bfc0d9076b6413
Michael L Perry <michael@qedcode.com>
2021-12-19 13:34:36 -0600

    Fixed Jinaga sourcemap

--- integration-test/run.sh
+++ integration-test/run.sh
@@ -2,7 +2,7 @@
 docker build -t jinaga-postgres-fact-keystore ./postgres
 
 cp ../dist/index.js ./jinaga-test/jinaga.js
-cp ../dist/index.js.map ./jinaga-test/jinaga.js.map
+cp ../dist/index.js.map ./jinaga-test/
 docker build -t jinaga-test ./jinaga-test
 
 docker compose up --exit-code-from jinaga-test --renew-anon-volumes
