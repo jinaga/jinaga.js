d2623127a9272caea2309604bf2a7e9fcc33077a
Michael L Perry <michael@qedcode.com>
2022-01-16 22:44:10 -0600

    Script to run the integration test database on its own.

---
+++ integration-test/start_db.sh
@@ -0,0 +1,1 @@
+docker run -d --name jinaga-postgres -p5432:5432 -e POSTGRES_PASSWORD=superuser -e APP_USERNAME=dev -e APP_PASSWORD=devpw -e APP_DATABASE=integrationtest jinaga-postgres-fact-keystore
