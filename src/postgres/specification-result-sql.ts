52c1244c255dc7fc7d20225850172673025ce604
Michael L Perry <michael@qedcode.com>
2022-07-17 14:44:31 -0500

    Extracted common code

--- src/postgres/specification-result-sql.ts
+++ src/postgres/specification-result-sql.ts
@@ -1,9 +1,10 @@
-import { FieldProjection, Label, Match, PathCondition, Projection, Specification, SpecificationProjection, ChildProjections, ResultProjection, SingularProjection } from "../specification/specification";
+import { FieldProjection, Match, Projection, Specification, SpecificationProjection, ChildProjections, ResultProjection, SingularProjection } from "../specification/specification";
 import { FactReference } from "../storage";
-import { FactTypeMap, getFactTypeId, getRoleId, RoleMap } from "./maps";
+import { FactTypeMap, getFactTypeId, RoleMap } from "./maps";
 import { FactDescription, InputDescription, QueryDescription, SpecificationSqlQuery } from "./query-description";
+import { QueryDescriptionBuilder } from "./query-description-builder";
 
-type FactByIdentifier = {
+export type FactByIdentifier = {
     [identifier: string]: FactDescription;
 };
 
@@ -181,11 +182,13 @@ interface NamedResultComposer {
     resultComposer: ResultComposer;
 }
 
-class ResultDescriptionBuilder {
+class ResultDescriptionBuilder extends QueryDescriptionBuilder {
     constructor(
-        private factTypes: FactTypeMap,
-        private roleMap: RoleMap
-    ) { }
+        factTypes: FactTypeMap,
+        roleMap: RoleMap
+    ) {
+        super(factTypes, roleMap);
+    }
 
     buildDescription(start: FactReference[], specification: Specification): ResultDescription {
         // Verify that the number of start facts equals the number of inputs
@@ -270,7 +273,7 @@ class ResultDescriptionBuilder {
         for (const match of matches) {
             for (const condition of match.conditions) {
                 if (condition.type === "path") {
-                    ({queryDescription, knownFacts} = this.addPathCondition(queryDescription, knownFacts, path, match.unknown, condition));
+                    ({queryDescription, knownFacts} = this.addPathCondition(queryDescription, knownFacts, path, match.unknown, "", condition));
                 }
                 else if (condition.type === "existential") {
                     if (condition.exists) {
@@ -305,102 +308,6 @@ class ResultDescriptionBuilder {
             queryDescription,
             knownFacts
         };
-    }
-
-    private addPathCondition(queryDescription: QueryDescription, knownFacts: FactByIdentifier, path: number[], unknown: Label, condition: PathCondition): { queryDescription: QueryDescription, knownFacts: FactByIdentifier } {
-        // If no input parameter has been allocated, allocate one now.
-        const input = queryDescription.inputByLabel(condition.labelRight);
-        if (input && input.factTypeParameter === 0) {
-            queryDescription = queryDescription.withInputParameter(input.label);
-        }
-
-        // Determine whether we have already written the output.
-        const knownFact = knownFacts[unknown.name];
-        const roleCount = condition.rolesLeft.length + condition.rolesRight.length;
-
-        // Walk up the right-hand side.
-        // This generates predecessor joins from a given or prior label.
-        let fact = knownFacts[condition.labelRight];
-        if (!fact) {
-            throw new Error(`Label ${condition.labelRight} not found. Known labels: ${Object.keys(knownFacts).join(", ")}`);
-        }
-        let type = fact.type;
-        let factIndex = fact.factIndex;
-        for (const [i, role] of condition.rolesRight.entries()) {
-            // If the type or role is not known, then no facts matching the condition can
-            // exist. The query is unsatisfiable.
-            const typeId = getFactTypeId(this.factTypes, type);
-            if (!typeId) {
-                return { queryDescription: QueryDescription.unsatisfiable, knownFacts };
-            }
-            const roleId = getRoleId(this.roleMap, typeId, role.name);
-            if (!roleId) {
-                return { queryDescription: QueryDescription.unsatisfiable, knownFacts };
-            }
-
-            const { query: queryWithParameter, parameterIndex: roleParameter } = queryDescription.withParameter(roleId);
-            if (i === roleCount - 1 && knownFact) {
-                // If we have already written the output, we can use the fact index.
-                queryDescription = queryWithParameter.withEdge(knownFact.factIndex, factIndex, roleParameter, path);
-                factIndex = knownFact.factIndex;
-            }
-            else {
-                // If we have not written the fact, we need to write it now.
-                const { query, factIndex: predecessorFactIndex } = queryWithParameter.withFact(role.targetType);
-                queryDescription = query.withEdge(predecessorFactIndex, factIndex, roleParameter, path);
-                factIndex = predecessorFactIndex;
-            }
-            type = role.targetType;
-        }
-
-        // Walk up the left-hand side.
-        // We will need to reverse this walk to generate successor joins.
-        type = unknown.type;
-        const newEdges: {
-            roleId: number,
-            declaringType: string,
-        }[] = [];
-        for (const role of condition.rolesLeft) {
-            // If the type or role is not known, then no facts matching the condition can
-            // exist. The query is unsatisfiable.
-            const typeId = getFactTypeId(this.factTypes, type);
-            if (!typeId) {
-                return { queryDescription: QueryDescription.unsatisfiable, knownFacts };
-            }
-            const roleId = getRoleId(this.roleMap, typeId, role.name);
-            if (!roleId) {
-                return { queryDescription: QueryDescription.unsatisfiable, knownFacts };
-            }
-
-            newEdges.push({
-                roleId,
-                declaringType: type
-            });
-            type = role.targetType;
-        }
-        newEdges.reverse().forEach(({ roleId, declaringType }, i) => {
-            const { query: queryWithParameter, parameterIndex: roleParameter } = queryDescription.withParameter(roleId);
-            if (condition.rolesRight.length + i === roleCount - 1 && knownFact) {
-                queryDescription = queryWithParameter.withEdge(factIndex, knownFact.factIndex, roleParameter, path);
-                factIndex = knownFact.factIndex;
-            }
-            else {
-                const { query: queryWithFact, factIndex: successorFactIndex } = queryWithParameter.withFact(declaringType);
-                queryDescription = queryWithFact.withEdge(factIndex, successorFactIndex, roleParameter, path);
-                factIndex = successorFactIndex;
-            }
-        });
-
-        // If we have not captured the known fact, add it now.
-        if (!knownFact) {
-            knownFacts = { ...knownFacts, [unknown.name]: { factIndex, type: unknown.type } };
-            // If we have not written the output, write it now.
-            // Only write the output if we are not inside of an existential condition.
-            if (path.length === 0) {
-                queryDescription = queryDescription.withOutput(unknown.name, unknown.type, factIndex);
-            }
-        }
-        return { queryDescription, knownFacts };
     }
 }
 
--- src/postgres/specification-sql.ts
+++ src/postgres/specification-sql.ts
@@ -1,16 +1,18 @@
 import { Label, Match, PathCondition, Projection, Specification } from "../specification/specification";
 import { FactBookmark, FactReference } from "../storage";
-import { getFactTypeId, getRoleId } from "./maps";
+import { FactTypeMap, getFactTypeId, getRoleId, RoleMap } from "./maps";
 import { FactDescription, InputDescription, QueryDescription, SpecificationSqlQuery } from "./query-description";
+import { QueryDescriptionBuilder } from "./query-description-builder";
 
 type FactByIdentifier = {
     [identifier: string]: FactDescription;
 };
 
-class DescriptionBuilder {
+class DescriptionBuilder extends QueryDescriptionBuilder {
     constructor(
-        private factTypes: Map<string, number>,
-        private roleMap: Map<number, Map<string, number>>) { }
+        factTypes: FactTypeMap,
+        roleMap: RoleMap
+    ) { super(factTypes, roleMap); }
 
     public buildDescriptions(start: FactReference[], specification: Specification): QueryDescription[] {
         // Verify that the number of start facts equals the number of inputs
@@ -109,100 +111,6 @@ class DescriptionBuilder {
         }
         queryDescriptions.push(queryDescription);
         return { queryDescriptions, knownFacts };
-    }
-
-    addPathCondition(queryDescription: QueryDescription, knownFacts: FactByIdentifier, path: number[], unknown: Label, prefix: string, condition: PathCondition): { queryDescription: QueryDescription, knownFacts: FactByIdentifier } {
-        // If no input parameter has been allocated, allocate one now.
-        const input = queryDescription.inputByLabel(condition.labelRight);
-        if (input && input.factTypeParameter === 0) {
-            queryDescription = queryDescription.withInputParameter(input.label);
-        }
-
-        // Determine whether we have already written the output.
-        const knownFact = knownFacts[unknown.name];
-        const roleCount = condition.rolesLeft.length + condition.rolesRight.length;
-
-        // Walk up the right-hand side.
-        // This generates predecessor joins from a given or prior label.
-        let fact = knownFacts[condition.labelRight];
-        let type = fact.type;
-        let factIndex = fact.factIndex;
-        for (const [i, role] of condition.rolesRight.entries()) {
-            // If the type or role is not known, then no facts matching the condition can
-            // exist. The query is unsatisfiable.
-            const typeId = getFactTypeId(this.factTypes, type);
-            if (!typeId) {
-                return { queryDescription: QueryDescription.unsatisfiable, knownFacts };
-            }
-            const roleId = getRoleId(this.roleMap, typeId, role.name);
-            if (!roleId) {
-                return { queryDescription: QueryDescription.unsatisfiable, knownFacts };
-            }
-
-            const { query: queryWithParameter, parameterIndex: roleParameter } = queryDescription.withParameter(roleId);
-            if (i === roleCount - 1 && knownFact) {
-                // If we have already written the output, we can use the fact index.
-                queryDescription = queryWithParameter.withEdge(knownFact.factIndex, factIndex, roleParameter, path);
-                factIndex = knownFact.factIndex;
-            }
-            else {
-                // If we have not written the fact, we need to write it now.
-                const { query, factIndex: predecessorFactIndex } = queryWithParameter.withFact(role.targetType);
-                queryDescription = query.withEdge(predecessorFactIndex, factIndex, roleParameter, path);
-                factIndex = predecessorFactIndex;
-            }
-            type = role.targetType;
-        }
-
-        // Walk up the left-hand side.
-        // We will need to reverse this walk to generate successor joins.
-        type = unknown.type;
-        const newEdges: {
-            roleId: number,
-            declaringType: string,
-        }[] = [];
-        for (const role of condition.rolesLeft) {
-            // If the type or role is not known, then no facts matching the condition can
-            // exist. The query is unsatisfiable.
-            const typeId = getFactTypeId(this.factTypes, type);
-            if (!typeId) {
-                return { queryDescription: QueryDescription.unsatisfiable, knownFacts };
-            }
-            const roleId = getRoleId(this.roleMap, typeId, role.name);
-            if (!roleId) {
-                return { queryDescription: QueryDescription.unsatisfiable, knownFacts };
-            }
-
-            newEdges.push({
-                roleId,
-                declaringType: type
-            });
-            type = role.targetType;
-        }
-        newEdges.reverse().forEach(({ roleId, declaringType }, i) => {
-            const { query: queryWithParameter, parameterIndex: roleParameter } = queryDescription.withParameter(roleId);
-            if (condition.rolesRight.length + i === roleCount - 1 && knownFact) {
-                queryDescription = queryWithParameter.withEdge(factIndex, knownFact.factIndex, roleParameter, path);
-                factIndex = knownFact.factIndex;
-            }
-            else {
-                const { query: queryWithFact, factIndex: successorFactIndex } = queryWithParameter.withFact(declaringType);
-                queryDescription = queryWithFact.withEdge(factIndex, successorFactIndex, roleParameter, path);
-                factIndex = successorFactIndex;
-            }
-        });
-
-        // If we have not captured the known fact, add it now.
-        if (!knownFact) {
-            knownFacts = { ...knownFacts, [unknown.name]: { factIndex, type: unknown.type } };
-            // If we have not written the output, write it now.
-            // Only write the output if we are not inside of an existential condition.
-            // Use the prefix, which will be set for projections.
-            if (path.length === 0) {
-                queryDescription = queryDescription.withOutput(prefix + unknown.name, unknown.type, factIndex);
-            }
-        }
-        return { queryDescription, knownFacts };
     }
 
     addProjections(queryDescription: QueryDescription, knownFacts: FactByIdentifier, projections: Projection[]): QueryDescription[] {
