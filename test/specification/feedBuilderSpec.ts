2f6aee3147606681efb0e26acdee42d46bcf71fc
Michael L Perry <michael@qedcode.com>
2022-09-01 19:39:48 -0500

    Test existential conditions

--- test/specification/feedBuilderSpec.ts
+++ test/specification/feedBuilderSpec.ts
@@ -119,6 +119,149 @@ describe("feed generator", () => {
 
         expect(feeds).toEqual(expectedFeeds);
     });
+
+    it("should accept existential conditions", () => {
+        const feeds = getFeeds(`
+            (user: Jinaga.User, root: Root) {
+                assignment: MyApp.Assignment [
+                    assignment->user:Jinaga.User = user
+                    assignment->project:MyApp.Project->root:Root = root
+                    !E {
+                        revoked: MyApp.Assignment.Revoked [
+                            revoked->assignment:MyApp.Assignment = assignment
+                        ]
+                    }
+                ]
+            }`);
+
+        const expectedFeeds: Feed[] = [
+            {
+                facts: [
+                    {
+                        factIndex: 1,
+                        factType: "Jinaga.User"
+                    },
+                    {
+                        factIndex: 2,
+                        factType: "MyApp.Assignment"
+                    },
+                    {
+                        factIndex: 3,
+                        factType: "Root"
+                    },
+                    {
+                        factIndex: 4,
+                        factType: "MyApp.Project"
+                    }
+                ],
+                inputs: [
+                    {
+                        factIndex: 1,
+                        factHash: user.hash
+                    },
+                    {
+                        factIndex: 3,
+                        factHash: root.hash
+                    }
+                ],
+                edges: [
+                    {
+                        edgeIndex: 1,
+                        predecessorFactIndex: 1,
+                        successorFactIndex: 2,
+                        roleName: "user"
+                    },
+                    {
+                        edgeIndex: 2,
+                        predecessorFactIndex: 3,
+                        successorFactIndex: 4,
+                        roleName: "root"
+                    },
+                    {
+                        edgeIndex: 3,
+                        predecessorFactIndex: 4,
+                        successorFactIndex: 2,
+                        roleName: "project"
+                    }
+                ],
+                outputs: [
+                    {
+                        factIndex: 2
+                    }
+                ]
+            },
+            {
+                facts: [
+                    {
+                        factIndex: 1,
+                        factType: "Jinaga.User"
+                    },
+                    {
+                        factIndex: 2,
+                        factType: "MyApp.Assignment"
+                    },
+                    {
+                        factIndex: 3,
+                        factType: "Root"
+                    },
+                    {
+                        factIndex: 4,
+                        factType: "MyApp.Project"
+                    },
+                    {
+                        factIndex: 5,
+                        factType: "MyApp.Assignment.Revoked"
+                    }
+                ],
+                inputs: [
+                    {
+                        factIndex: 1,
+                        factHash: user.hash
+                    },
+                    {
+                        factIndex: 3,
+                        factHash: root.hash
+                    }
+                ],
+                edges: [
+                    {
+                        edgeIndex: 1,
+                        predecessorFactIndex: 1,
+                        successorFactIndex: 2,
+                        roleName: "user"
+                    },
+                    {
+                        edgeIndex: 2,
+                        predecessorFactIndex: 3,
+                        successorFactIndex: 4,
+                        roleName: "root"
+                    },
+                    {
+                        edgeIndex: 3,
+                        predecessorFactIndex: 4,
+                        successorFactIndex: 2,
+                        roleName: "project"
+                    },
+                    {
+                        edgeIndex: 4,
+                        predecessorFactIndex: 2,
+                        successorFactIndex: 5,
+                        roleName: "assignment"
+                    }
+                ],
+                outputs: [
+                    {
+                        factIndex: 2
+                    },
+                    {
+                        factIndex: 5
+                    }
+                ]
+            }
+        ];
+
+        expect(feeds).toEqual(expectedFeeds);
+    });
 });
 
 const root = dehydrateReference({ type: 'Root' });
