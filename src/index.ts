ef7f0e17e9ed3a4552a598cfc7a7e8d4db3926f6
Michael L Perry <michael@qedcode.com>
2022-09-04 15:03:03 -0500

    Publish FeedResponse

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ export { Fork } from "./fork/fork";
 export { PassThroughFork } from "./fork/pass-through-fork";
 export { TransientFork } from './fork/transient-fork';
 export {
+  FeedResponse,
   FeedsResponse,
   LoadMessage,
   LoadResponse,
