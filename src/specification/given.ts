493215eb40b4b4d5d9df87abc21d636c6c11bb31
Michael L Perry <michael@qedcode.com>
2022-12-03 18:11:28 -0600

    Add remaining field types to specification result.

--- src/specification/given.ts
+++ src/specification/given.ts
@@ -70,9 +70,12 @@ export class SpecificationOf<T, U> {
 type MatchParameters<T> = T extends [ infer First, ...infer Rest ] ? [ Label<First>, ...MatchParameters<Rest> ] : [ FactRepository ];
 type SpecificationResult<U> =
     U extends string ? U :
+    U extends number ? U :
+    U extends boolean ? U :
+    U extends Date ? U :
     U extends Label<infer V> ? V :
     U extends Traversal<infer V> ? Array<SpecificationResult<V>> :
-    U extends {} ? { [K in keyof U]: SpecificationResult<U[K]> } :
+    U extends object ? { [K in keyof U]: SpecificationResult<U[K]> } :
     U;
 
 class Given<T extends any[]> {
