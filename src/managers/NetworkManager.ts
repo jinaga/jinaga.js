48835aca5d86ea0546b78acc9404a9ac9e1e1863
Michael L Perry <michael@qedcode.com>
2023-03-04 16:19:30 -0600

    Load the unknown facts as soon as possible.

--- src/managers/NetworkManager.ts
+++ src/managers/NetworkManager.ts
@@ -1,6 +1,7 @@
 import { FeedResponse } from "../http/messages";
 import { Specification } from "../specification/specification";
 import { FactEnvelope, FactReference, factReferenceEquals, Storage } from "../storage";
+import { Trace } from "../util/trace";
 
 export interface Network {
     feeds(start: FactReference[], specification: Specification): Promise<string[]>;
@@ -23,10 +24,67 @@ export class NetworkNoOp implements Network {
     }
 }
 
+class LoadBatch {
+    private readonly factReferences: FactReference[] = [];
+    private started = false;
+    public readonly completed: Promise<void>;
+    private resolve: (() => void) | undefined;
+    private reject: ((reason: any) => void) | undefined;
+    private timeout: NodeJS.Timeout;
+
+    constructor(
+        private readonly network: Network,
+        private readonly store: Storage,
+        private readonly notifyFactsAdded: (factsAdded: FactEnvelope[]) => Promise<void>,
+        private readonly onRun: () => void
+    ) {
+        this.completed = new Promise<void>((resolve, reject) => {
+            this.resolve = resolve;
+            this.reject = reject;
+        });
+        this.timeout = setTimeout(() => {
+            Trace.info('Trigger batch on timeout');
+            this.run();
+            this.onRun();
+        }, 100);
+    }
+
+    add(factReferences: FactReference[]) {
+        for (const fr of factReferences) {
+            if (!this.factReferences.some(factReferenceEquals(fr))) {
+                this.factReferences.push(fr);
+            }
+        }
+    }
+
+    trigger() {
+        clearTimeout(this.timeout);
+        this.run();
+        this.onRun();
+    }
+
+    private run() {
+        if (!this.started) {
+            this.load()
+                .then(this.resolve)
+                .catch(this.reject);
+            this.started = true;
+        }
+    }
+
+    private async load() {
+        const graph: FactEnvelope[] = await this.network.load(this.factReferences);
+
+        const factsAdded = await this.store.save(graph);
+
+        await this.notifyFactsAdded(factsAdded);
+    }
+}
+
 export class NetworkManager {
     private activeFeeds = new Map<string, Promise<void>>();
-    private loadBatch: FactReference[] = [];
-    private loadCompleted: Promise<void> | null = null;
+    private fectchCount = 0;
+    private currentBatch: LoadBatch | null = null;
 
     constructor(
         private readonly network: Network,
@@ -55,55 +113,59 @@ export class NetworkManager {
         let bookmark: string = await this.store.loadBookmark(feed);
 
         while (true) {
-            const { references: factReferences, bookmark: nextBookmark } = await this.network.fetchFeed(feed, bookmark);
+            this.fectchCount++;
+            Trace.metric('Fetch begin', { fectchCount: this.fectchCount });
+            let decremented = false;
+            try {
+                const { references: factReferences, bookmark: nextBookmark } = await this.network.fetchFeed(feed, bookmark);
 
-            if (factReferences.length === 0) {
-                break;
+                if (factReferences.length === 0) {
+                    Trace.info('End of feed');
+                    break;
+                }
+
+                const knownFactReferences: FactReference[] = await this.store.whichExist(factReferences);
+                const unknownFactReferences: FactReference[] = factReferences.filter(fr => !knownFactReferences.includes(fr));
+                if (unknownFactReferences.length > 0) {
+                    let batch = this.currentBatch;
+                    if (batch === null) {
+                        // Begin a new batch.
+                        Trace.info('Begin batch');
+                        batch = new LoadBatch(this.network, this.store, this.notifyFactsAdded, () => {
+                            if (this.currentBatch === batch) {
+                                this.currentBatch = null;
+                            }
+                        });
+                        this.currentBatch = batch;
+                    }
+                    batch.add(unknownFactReferences);
+                    this.fectchCount--;
+                    Trace.metric('Fetch end', { fectchCount: this.fectchCount });
+                    decremented = true;
+                    if (this.fectchCount === 0) {
+                        // This is the last fetch, so trigger the batch.
+                        Trace.info('Trigger batch on last fetch');
+                        batch.trigger();
+                    }
+                    await batch.completed;
+                }
+
+                bookmark = nextBookmark;
+                await this.store.saveBookmark(feed, bookmark);
             }
-
-            const knownFactReferences: FactReference[] = await this.store.whichExist(factReferences);
-            const unknownFactReferences: FactReference[] = factReferences.filter(fr => !knownFactReferences.includes(fr));
-            if (unknownFactReferences.length > 0) {
-                await this.loadInBatch(unknownFactReferences);
+            finally {
+                if (!decremented) {
+                    this.fectchCount--;
+                    Trace.metric('Fetch aborted', { fectchCount: this.fectchCount });
+                    if (this.fectchCount === 0 && this.currentBatch !== null) {
+                        // This is the last fetch, so trigger the batch.
+                        Trace.info('Trigger batch on last fetch');
+                        this.currentBatch.trigger();
+                    }
+                }
             }
-
-            bookmark = nextBookmark;
-            await this.store.saveBookmark(feed, bookmark);
         }
 
         this.activeFeeds.delete(feed);
-    }
-
-    private loadInBatch(factReferences: FactReference[]) {
-        // Add the fact references that are not already in the batch.
-        for (const fr of factReferences) {
-            if (!this.loadBatch.some(factReferenceEquals(fr))) {
-                this.loadBatch.push(fr);
-            }
-        }
-
-        // Start a new batch if one is not already waiting.
-        if (this.loadCompleted === null) {
-            this.loadCompleted = new Promise<void>((resolve, reject) => {
-                setTimeout(() => {
-                    // Prepare to start a new batch.
-                    const loadBatch = this.loadBatch;
-                    this.loadBatch = [];
-                    this.loadCompleted = null;
-                    this.loadAndSave(loadBatch)
-                        .then(resolve)
-                        .catch(reject);
-                }, 100);
-            });
-        }
-        return this.loadCompleted;
-    }
-
-    private async loadAndSave(factReferences: FactReference[]) {
-        const graph: FactEnvelope[] = await this.network.load(factReferences);
-
-        const factsAdded = await this.store.save(graph);
-
-        await this.notifyFactsAdded(factsAdded);
     }
 }
