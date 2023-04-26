9070b1789ac5c535bbb06afb9da4984a9c6f1436
Michael L Perry <michael@qedcode.com>
2023-04-25 21:18:08 -0500

    ProjectionOf utility type

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,7 @@ export { Declaration } from './specification/declaration';
 export { describeDeclaration, describeSpecification } from './specification/description';
 export { EdgeDescription, FactDescription, Feed, InputDescription, NotExistsConditionDescription, OutputDescription, getAllFactTypesFromFeed, getAllRolesFromFeed } from './specification/feed';
 export { buildFeeds } from './specification/feed-builder';
-export { FactRepository, LabelOf, Model, ModelBuilder, SpecificationOf, buildModel } from './specification/model';
+export { FactRepository, LabelOf, Model, ModelBuilder, ProjectionOf, SpecificationOf, buildModel } from './specification/model';
 export { ComponentProjection, CompositeProjection, FactProjection, FieldProjection, HashProjection, Label, Match, PathCondition, Projection, SingularProjection, Specification, SpecificationProjection, getAllFactTypes, getAllRoles } from './specification/specification';
 export { SpecificationParser } from './specification/specification-parser';
 export { FactEnvelope, FactFeed, FactPath, FactRecord, FactReference, FactSignature, FactTuple, PredecessorCollection, ProjectedResult, ReferencesByName, Storage, factReferenceEquals } from './storage';
