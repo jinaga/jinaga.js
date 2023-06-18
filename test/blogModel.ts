126b7a95f1ba041b13f6a7f3fa298bae30a92471
Michael L Perry <michael@qedcode.com>
2023-06-17 21:55:56 -0500

    Test distribution rules with given user

--- test/blogModel.ts
+++ test/blogModel.ts
@@ -33,6 +33,28 @@ export class Publish {
   ) { }
 }
 
+export class Comment {
+  static Type = "Comment" as const;
+  type = Comment.Type;
+
+  constructor(
+    public post: Post,
+    public author: User,
+    public text: string,
+    public createdAt: Date | string
+  ) { }
+}
+
+export class CommentApproved {
+  static Type = "CommentApproved" as const;
+  type = CommentApproved.Type;
+
+  constructor(
+    public comment: Comment,
+    public approvedAt: Date | string
+  ) { }
+}
+
 export const model = buildModel(b => b
   .type(User)
   .type(Blog, x => x
@@ -45,9 +67,17 @@ export const model = buildModel(b => b
   .type(Publish, x => x
     .predecessor("post", Post)
   )
+  .type(Comment, x => x
+    .predecessor("post", Post)
+    .predecessor("author", User)
+  )
+  .type(CommentApproved, x => x
+    .predecessor("comment", Comment)
+  )
 );
 
 export const distribution = (r: DistributionRules) => r
+  // Everyone can see published posts
   .everyone(model.given(Blog).match((blog, facts) =>
     facts.ofType(Post)
       .join(post => post.blog, blog)
@@ -55,6 +85,7 @@ export const distribution = (r: DistributionRules) => r
         .join(publish => publish.post, post)
       )
   ))
+  // The creator can see all posts
   .only(model.given(Blog).match((blog, facts) =>
     facts.ofType(Post)
       .join(post => post.blog, blog)
@@ -62,6 +93,33 @@ export const distribution = (r: DistributionRules) => r
     model.given(Blog).match((blog, facts) =>
       facts.ofType(User)
         .join(user => user, blog.creator)
+    )
+  )
+  // The creator can see all comments
+  .only(model.given(Blog).match((blog, facts) =>
+    facts.ofType(Post)
+      .join(post => post.blog, blog)
+      .selectMany(post => facts.ofType(Comment)
+        .join(comment => comment.post, post)
+      )
+  ),
+    model.given(Blog).match((blog, facts) =>
+      facts.ofType(User)
+        .join(user => user, blog.creator)
+    )
+  )
+  // A comment author can see their own comments
+  .only(model.given(Blog, User).match((blog, author, facts) =>
+    facts.ofType(Post)
+      .join(post => post.blog, blog)
+      .selectMany(post => facts.ofType(Comment)
+        .join(comment => comment.post, post)
+        .join(comment => comment.author, author)
+      )
+  ),
+    model.given(Blog, User).match((blog, author, facts) =>
+      facts.ofType(User)
+        .join(user => user, author)
     )
   )
   ;
--- test/distribution/distributionRuleSpec.ts
+++ test/distribution/distributionRuleSpec.ts
@@ -1,11 +1,17 @@
+import { Trace } from "../../src/jinaga";
 import { JinagaTest } from "../../src/jinaga-test";
 import { User } from "../../src/model/user";
-import { Blog, Post, Publish, distribution, model } from "../blogModel";
+import { Blog, Comment, Post, Publish, distribution, model } from "../blogModel";
 
 describe("distribution rules", () => {
+  Trace.off();
+
   const creator = new User("creator");
   const reader = new User("reader");
+  const commenter = new User("commenter");
   const blog = new Blog(creator, "domain");
+  const post = new Post(blog, creator, new Date());
+  const comment = new Comment(post, commenter, "text", new Date());
 
   it("should prevent public access to unpublished posts", async () => {
     const specification = model.given(Blog).match((blog, facts) =>
@@ -14,7 +20,7 @@ describe("distribution rules", () => {
     );
 
     const j = givenLoggedIn(undefined);
-    await expect(() => j.query(specification, blog)).rejects;
+    await expect(() => j.query(specification, blog)).rejects.toThrow("not authorized");
   });
 
   it("should permit public access to publications", async () => {
@@ -24,7 +30,8 @@ describe("distribution rules", () => {
     );
 
     const j = givenLoggedIn(undefined);
-    await expect(() => j.query(specification, blog)).resolves;
+    const result = await j.query(specification, blog);
+    expect(result).toHaveLength(0);
   });
 
   it("should permit public access to published posts", async () => {
@@ -37,7 +44,8 @@ describe("distribution rules", () => {
     );
 
     const j = givenLoggedIn(undefined);
-    await expect(() => j.query(specification, blog)).rejects;
+    const result = await j.query(specification, blog);
+    expect(result).toHaveLength(0);
   });
 
   it("should permit the creator to access all posts", async () => {
@@ -47,7 +55,8 @@ describe("distribution rules", () => {
     );
 
     const j = givenLoggedIn(creator);
-    await expect(() => j.query(specification, blog)).resolves;
+    const result = await j.query(specification, blog);
+    expect(result).toHaveLength(1);
   });
 
   it("should not permit a reader to access all posts", async () => {
@@ -57,7 +66,7 @@ describe("distribution rules", () => {
     );
 
     const j = givenLoggedIn(reader);
-    await expect(() => j.query(specification, blog)).rejects;
+    await expect(() => j.query(specification, blog)).rejects.toThrow("not authorized");
   });
 
   it("should permit reader to access publications", async () => {
@@ -67,7 +76,8 @@ describe("distribution rules", () => {
     );
 
     const j = givenLoggedIn(reader);
-    await expect(() => j.query(specification, blog)).resolves;
+    const result = await j.query(specification, blog);
+    expect(result).toHaveLength(0);
   });
 
   it("should permit reader to access published posts", async () => {
@@ -80,7 +90,72 @@ describe("distribution rules", () => {
     );
 
     const j = givenLoggedIn(reader);
-    await expect(() => j.query(specification, blog)).resolves;
+    const result = await j.query(specification, blog);
+    expect(result).toHaveLength(0);
+  });
+
+  it("should permit creator to access all comments", async () => {
+    const specification = model.given(Blog).match((blog, facts) =>
+      facts.ofType(Comment)
+        .join(comment => comment.post.blog, blog)
+    );
+
+    const j = givenLoggedIn(creator);
+    const result = await j.query(specification, blog);
+    expect(result).toHaveLength(1);
+  });
+
+  it(("should not permit public access to comments"), async () => {
+    const specification = model.given(Blog).match((blog, facts) =>
+      facts.ofType(Comment)
+        .join(comment => comment.post.blog, blog)
+    );
+
+    const j = givenLoggedIn(undefined);
+    await expect(() => j.query(specification, blog)).rejects.toThrow("not authorized");
+  });
+
+  it("should not permit reader to access all comments", async () => {
+    const specification = model.given(Blog).match((blog, facts) =>
+      facts.ofType(Comment)
+        .join(comment => comment.post.blog, blog)
+    );
+
+    const j = givenLoggedIn(reader);
+    await expect(() => j.query(specification, blog)).rejects.toThrow("not authorized");
+  });
+
+  it("should not permit commenter to access all comments", async () => {
+    const specification = model.given(Blog).match((blog, facts) =>
+      facts.ofType(Comment)
+        .join(comment => comment.post.blog, blog)
+    );
+
+    const j = givenLoggedIn(commenter);
+    await expect(() => j.query(specification, blog)).rejects.toThrow("not authorized");
+  });
+
+  it("should permit commenter to access their own comments", async () => {
+    const specification = model.given(Blog, User).match((blog, commenter, facts) =>
+      facts.ofType(Comment)
+        .join(comment => comment.post.blog, blog)
+        .join(comment => comment.author, commenter)
+    );
+
+    const j = givenLoggedIn(commenter);
+    const result = await j.query(specification, blog, commenter);
+    expect(result).toHaveLength(1);
+  });
+
+  it("should not permit reader to access someone else's comments", async () => {
+    const specification = model.given(Blog, User).match((blog, commenter, facts) =>
+      facts.ofType(Comment)
+        .join(comment => comment.post.blog, blog)
+        .join(comment => comment.author, commenter)
+    );
+
+    const j = givenLoggedIn(reader);
+    await expect(() => j.query(specification, blog, commenter)).rejects.toThrow("not authorized");
   });
 
   function givenLoggedIn(user: User | undefined) {
@@ -90,7 +165,10 @@ describe("distribution rules", () => {
       initialState: [
         creator,
         reader,
-        blog
+        commenter,
+        blog,
+        post,
+        comment
       ],
       distribution
     });
