5fb6d731d2bc7920cd44901c7bea2f683d4e383c
Michael L Perry <michael@qedcode.com>
2024-05-25 14:14:33 -0500

    Optimize to reduce the number of times public keys are parsed

--- src/cryptography/verify.ts
+++ src/cryptography/verify.ts
@@ -3,11 +3,24 @@ import { canonicalizeFact } from "../fact/hash";
 import { FactEnvelope, FactSignature } from "../storage";
 import { Trace } from "../util/trace";
 
-export function verifyEnvelopes(envelope: FactEnvelope[]): boolean {
-    return envelope.every(e => verifySignatures(e));
+type PublicKeyCache = { [key: string]: pki.rsa.PublicKey };
+
+export function verifyEnvelopes(envelopes: FactEnvelope[]): boolean {
+    // Cache public keys to avoid parsing them multiple times
+    const publicKeyCache: PublicKeyCache = {};
+
+    for (var envelope of envelopes) {
+        for (var signature of envelope.signatures) {
+            if (!publicKeyCache[signature.publicKey]) {
+                publicKeyCache[signature.publicKey] = pki.publicKeyFromPem(signature.publicKey);
+            }
+        }
+    }
+
+    return envelopes.every(e => verifySignatures(e, publicKeyCache));
 }
 
-function verifySignatures(envelope: FactEnvelope): boolean {
+function verifySignatures(envelope: FactEnvelope, publicKeyCache: PublicKeyCache): boolean {
     const canonicalString = canonicalizeFact(envelope.fact.fields, envelope.fact.predecessors);
     const encodedString = util.encodeUtf8(canonicalString);
     const digest = md.sha512.create().update(encodedString);
@@ -17,11 +30,11 @@ function verifySignatures(envelope: FactEnvelope): boolean {
         return false;
     }
     const digestBytes = digest.digest().getBytes();
-    return envelope.signatures.every(s => verifySignature(s, digestBytes));
+    return envelope.signatures.every(s => verifySignature(s, digestBytes, publicKeyCache));
 }
 
-function verifySignature(signature: FactSignature, digestBytes: string) {
-    const publicKey = pki.publicKeyFromPem(signature.publicKey);
+function verifySignature(signature: FactSignature, digestBytes: string, publicKeyCache: PublicKeyCache) {
+    const publicKey = publicKeyCache[signature.publicKey];
     const signatureBytes = util.decode64(signature.signature);
     try {
         return publicKey.verify(digestBytes, signatureBytes);
