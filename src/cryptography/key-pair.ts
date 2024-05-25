ed19c75b57e8a821e3177255d41d5f0ef6587ec2
Michael L Perry <michael@qedcode.com>
2024-05-25 09:31:12 -0500

    Bring in implementation of key generation and fact signing

--- src/cryptography/key-pair.ts
+++ src/cryptography/key-pair.ts
@@ -1,4 +1,7 @@
+import { md, pki, util } from "node-forge";
+import { canonicalizeFact } from "../fact/hash";
 import { FactEnvelope, FactRecord } from "../storage";
+import { Trace } from "../util/trace";
 
 export interface KeyPair {
     publicPem: string;
@@ -6,9 +9,36 @@ export interface KeyPair {
 }
 
 export function generateKeyPair(): KeyPair {
-    throw new Error("Not implemented");
+    const keypair = pki.rsa.generateKeyPair({ bits: 2048 });
+    const privatePem = pki.privateKeyToPem(keypair.privateKey);
+    const publicPem = pki.publicKeyToPem(keypair.publicKey);
+    return { privatePem, publicPem };
 }
 
 export function signFacts(keyPair: KeyPair, facts: FactRecord[]): FactEnvelope[] {
-    throw new Error("Not implemented");
+    const privateKey = <pki.rsa.PrivateKey>pki.privateKeyFromPem(keyPair.privatePem);
+    const envelopes: FactEnvelope[] = facts.map(fact => signFact(fact, keyPair.publicPem, privateKey));
+    return envelopes;
+}
+
+function signFact(fact: FactRecord, publicPem: string, privateKey: pki.rsa.PrivateKey): FactEnvelope {
+    const canonicalString = canonicalizeFact(fact.fields, fact.predecessors);
+    const encodedString = util.encodeUtf8(canonicalString);
+    const digest = md.sha512.create().update(encodedString);
+    const hash = util.encode64(digest.digest().getBytes());
+    if (fact.hash !== hash) {
+        Trace.error(`Hash does not match. "${fact.hash}" !== "${hash}"\nFact: ${canonicalString}`);
+        return {
+            fact,
+            signatures: []
+        };
+    }
+    const signature = util.encode64(privateKey.sign(digest));
+    return {
+        fact,
+        signatures: [{
+            signature,
+            publicKey: publicPem
+        }]
+    };
 }
