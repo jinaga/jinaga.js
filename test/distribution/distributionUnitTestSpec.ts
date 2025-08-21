19c5c744a1a5d3caaf1d6b794c55233ae3c882ee
Michael L Perry <michael@qedcode.com>
2025-08-20 21:20:16 -0500

    Replicate the issue

---
+++ test/distribution/distributionUnitTestSpec.ts
@@ -0,0 +1,48 @@
+import { buildModel, DistributionRules, JinagaTest, LabelOf, User } from "../../src";
+
+describe("Distribution rules in unit tests", () => {
+    it("should pass when distribution rule allows", async () => {
+        const loggedInUser = new User("user1");
+        const jinaga = JinagaTest.create({
+            model,
+            user: loggedInUser,
+            distribution,
+            initialState: [
+                loggedInUser
+            ]
+        });
+
+        const namesSpec = model.given(User).match(user => UserName.current(user));
+        const names = await jinaga.query(namesSpec, loggedInUser);
+        expect(names).toStrictEqual([]);
+    });
+});
+
+class UserName {
+    static Type = "UserName" as const;
+    type = UserName.Type;
+
+    constructor(
+        public user: User,
+        public value: string,
+        public prior: UserName[]) {}
+
+    static current(user: LabelOf<User>) {
+        return user.successors(UserName, userName => userName.user)
+            .notExists(userName => userName.successors(UserName, next => next.prior));
+    }
+}
+
+const model = buildModel(b => b
+    .type(User)
+    .type(UserName, f => f
+        .predecessor("user", User)
+        .predecessor("prior", UserName)
+    )
+);
+
+function distribution(r: DistributionRules) {
+    return r
+        .share(model.given(User).match(user => UserName.current(user)))
+        .with(model.given(User).match(user => user));
+}
